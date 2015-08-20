22cddb0f9e60d6ae8a4face7a50f30976ee78f15
jdcroos <jeroen.decroos@gmail.com>
2015-08-20 21:28:18 +0000

    solved unicode problem

--- get_pronunciations.py
+++ get_pronunciations.py
@@ -1,7 +1,13 @@
 import sys
 import codecs
+import re
 
-phone  = sys.argv[1]
+phone_file  = sys.argv[1]
+
+with codecs.open(phone_file, 'rb','utf8') as instream:
+  phonelist = eval(instream.read())
+
+
 if len(sys.argv) > 2:
   n = int(sys.argv[2])
 else: 
@@ -13,20 +19,29 @@ with codecs.open('output_dict.txt', 'rb','utf8') as instream:
 with codecs.open('freq_list.out.txt', 'rb' , 'utf8') as instream:
   f_list = [x.strip() for x in instream]
 
-n_found=0
-for f in f_list:
-  if f not in w_dict:
-     continue
-  else:
-     pronunciations = w_dict[f]
-     found = False
-     for x in pronunciations:
-        if phone in x:
-            found = True
+for w,ps in w_dict.iteritems():
+   for pn in range(len(ps)):
+       ps[pn] =re.sub('\(.*','', ps[pn])
+
+def find_examples(phone):
+  n_found=0
+  for f in f_list:
+    if f not in w_dict:
+       continue
+    else:
+       pronunciations = w_dict[f]
+       found = False
+       for x in pronunciations:
+          if re.search(phone,x):
+              found = True
+              break
+       if found:
+             print(f+'\t' + ' , '.join(pronunciations))
+             n_found += 1
+       if n_found ==n:
             break
-     if found:
-           print(f+'\t'+str(pronunciations))
-           n_found += 1
-     if n_found ==n:
-          break
-  
+
+for x in phonelist:
+  print '\n'
+  print x
+  find_examples(x)    
