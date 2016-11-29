d4a7eef7fae0b06cd56e0c881993f2bbd469a028
jdcroos <jd_komoon@hotmail.com>
2016-11-29 07:55:49 -0800

    create unittests for phoneme data collector

--- src/test_get_phonemes.py
+++ src/test_get_phonemes.py
@@ -1,13 +1,32 @@
+import os
 import unittest
 import tempfile
-import os
 
 import tests.testlib.testcase as testcase
-import get_phonemes
+import pronunciation_master.src.get_phonemes as get_phonemes
+
+def create_phoneme_data_source_phiobe(filepath, language, phonemes):
+    with open(filepath, 'wb') as filestream:
+        filestream.write('LanguageCode\tPhoneme\n')
+        for phoneme in phonemes:
+            filestream.write('{}\t{}\n'.format(language, phoneme))
 
 
-class BlaBla(testcase.BaseTestCase):
-    pass
+class PhonemesTest(testcase.BaseTestCase):
+    def setUp(self):
+        _, self.temporary_source = tempfile.mkstemp()
+
+    def tearDown(self):
+        os.remove(self.temporary_source)
+
+    def test_get_all_phonemes(self):
+        phonemes = get_phonemes.PhonemesCollector('nld')
+        phoneme_list = ['p1', 'p2']
+        create_phoneme_data_source_phiobe(self.temporary_source, 'nld', phoneme_list)
+        phonemes.parse_source(self.temporary_source)
+        ret = phonemes.get_all_phonemes()
+        self.assertItemsEqual(ret, phoneme_list)
+
 
 if __name__ == '__main__':
     unittest.main()
