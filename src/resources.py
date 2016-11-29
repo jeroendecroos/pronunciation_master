43e93dfd81df1718f6a4bf8758d22e7badd328d5
jdcroos <jd_komoon@hotmail.com>
2016-11-29 07:58:47 -0800

    add phoible resources

--- src/resources.py
+++ src/resources.py
@@ -1,1 +1,4 @@
+import os
+DATADIR = '{}/data/'.format(os.getcwd())
 hermit_dave_github = "https://raw.github.com/hermitdave/FrequencyWords/master/content/2016/"
+phoible_database = "{}/phoible-by-phoneme.tsv".format(DATADIR)
