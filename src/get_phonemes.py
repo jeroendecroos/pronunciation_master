5aaf51a00f6c17d8142131300662ee7eebc302a9
jdcroos <jd_komoon@hotmail.com>
2016-11-29 09:14:07 -0800

    implement PhonemesCollector

--- src/get_phonemes.py
+++ src/get_phonemes.py
@@ -1,10 +1,33 @@
 """ get_frequency_master gets the most frequent words for a language
 """
 import argparse
-import StringIO
-import requests
+import csv
 
 import resources
+
+class PhonemesCollector(object):
+    def __init__(self, language):
+        self.language = language
+        self.language_code = self._get_language_code()
+        self.all_data = []
+
+    def parse_source(self, filepath):
+        """ Use a source to populate the Phonemes object with data
+        currently parsed from a fixed file, could benefit from an SQL like database"""
+        with open(filepath, 'rb') as tsv_file:
+            tsv_reader = csv.DictReader(tsv_file, delimiter='\t')
+            for row in tsv_reader:
+                if row['LanguageCode'] == self.language_code:
+                    self._add_datarow(row)
+
+    def _add_datarow(self, row):
+        self.all_data.append(row)
+
+    def _get_language_code(self):
+        return 'nld'
+
+    def get_all_phonemes(self):
+        return set(row['Phoneme'] for row in self.all_data)
 
 def get_phonemes(language):
     return []
