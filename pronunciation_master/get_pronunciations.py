fcd6916c5ec97388dc35242dd2ea4c8b7173621d
Jeroen Decroos <jeroen.decroos@gmail.com>
2016-12-05 22:38:24 +0100

    add initial files get_pronunciations

---
+++ pronunciation_master/get_pronunciations.py
@@ -0,0 +1,47 @@
+""" get_pronunciations gets the pronunciations for a word in a language
+"""
+import argparse
+
+from wiktionaryparser import WiktionaryParser
+
+import language_codes
+import resources
+
+def get_pronunciations(language, word):
+    wiktionary_entry = get_wiktionary_entry(language, word)
+    pronunciation_entries = filter_pronunciations(wiktionary_entry)
+    pronunciations = list_pronunciations(pronunciation_entries)
+    return pronunciations
+
+def get_wiktionary_entry(language, word):
+    parser = WiktionaryParser()
+    parser.set_default_language(language)
+    return parser.fetch(word)
+
+def filter_pronunciations(wiktionary_entry):
+    pass
+
+def list_pronunciations(pronunciation_entries):
+    pass
+
+def _parse_arguments():
+    """ parse the arguments from the commandline
+
+    Arguments:
+        None
+    Returns:
+        Namespace
+    """
+    parser = argparse.ArgumentParser(description='Get the phonemes from a language')
+    parser.add_argument('--language', dest='language', required=True,
+        help='the language we want the pronuncations for')
+    parser.add_argument('--word', dest='word', required=True,
+        help='the word we want the pronunciations for')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = _parse_arguments()
+    pronunciations = get_pronunciations(args.language, args.world)
+    for pronunciation in pronunciations:
+        print(pronunciation)
