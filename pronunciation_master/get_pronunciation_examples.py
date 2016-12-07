f5ee255301828a00d41f4754ffffd2418791101e
jdcroos <jd_komoon@hotmail.com>
2016-12-07 08:39:38 -0800

    create unittest pronunciation_examples

--- pronunciation_master/get_pronunciation_examples.py
+++ pronunciation_master/get_pronunciation_examples.py
@@ -2,10 +2,42 @@
 """
 import argparse
 
-import language_codes
+import get_phonemes
+import get_pronunciations
+import get_frequent_words
+
+
+class PronunciationExamples(object):
+    def __init__(self, phonemes):
+        self._examples = {key: [] for key in phonemes}
+
+    def add_pronunciations(self, word, pronunciations):
+        pass
+    
+    def __getitem__(self, name):
+        return self._examples[name]
+
+    def __iter__(self):
+        return iter(self._examples)
+
+    def keys(self):
+        return self._examples.keys()
+
+    def items(self):
+        return self._examples.items()
+
+    def values(self):
+        return self._examples.values()
 
 def get_pronunciation_examples(language):
-    pass
+    phonemes = get_phonemes.get_phonemes(language)
+    frequent_words = get_frequent_words.get_frequency_list(language)
+    examples = PronunciationExamples(phonemes)
+    for word in frequent_words:
+        pronunciations = get_pronunciations.get_pronunciations(language, word)
+        if pronunciations:
+            examples.add_pronunciations(word, pronunciations)
+    return examples
 
 def _parse_arguments():
     """ parse the arguments from the commandline
--- pronunciation_master/test_get_pronunciation_examples.py
+++ pronunciation_master/test_get_pronunciation_examples.py
@@ -1,10 +1,53 @@
 import unittest
+from nose2.tools import params
 
 import tests.testlib.testcase as testcase
 import get_pronunciation_examples
 
+available_phonemes = ['a','b','c']
+
 class PronunciationExamplesTest(testcase.BaseTestCase):
-    pass
+    def setUp(self):
+        self.test_class = get_pronunciation_examples.PronunciationExamples
+
+    def test__init__(self):
+        examples = self.test_class(['a'])
+
+    def test_keys(self):
+        examples = self.test_class(['a'])
+        self.assertItemsEqual(examples.keys(), 'a')
+
+    def test_items(self):
+        examples = self.test_class(['a'])
+        self.assertItemsEqual(examples.items(), ('a', []))
+
+    def test_values(self):
+        examples = self.test_class(['a'])
+        self.assertItemsEqual(examples.items(), [[]])
+
+    @params(
+            ('1 pronunciation, 1phoneme',
+                ('one', ['a']),
+                {'a':['one'], 'b':[], 'c':[]}
+            ),
+            ('1 pronunciation, 2phoneme',
+                ('one', ['ab']),
+                {'a':['one'], 'b':['one'], 'c':[]}
+            ),
+            ('2 pronunciation, different phonemes',
+                ('one', ['a', 'b']),
+                {'a':[], 'b':[], 'c':[]}
+            ),
+            ('2 pronunciation, 1 common phoneme',
+                ('one', ['ac', 'bc']),
+                {'a':[], 'b':[], 'c':['one']}
+            ))
+    def test_add_once_pronunciations(self, name, entry, expected ):
+        examples = self.test_class(available_phonemes)
+        examples.add_pronunciations(*entry)
+        self.assertItemsEqual(examples.items(), expected)
+
+
 
 if __name__ == '__main__':
     unittest.main()
