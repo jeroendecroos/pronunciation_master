537aecda58a7dbc31dc46a989b9a0f6243c57f34
Jeroen Decroos <jeroen.decroos@gmail.com>
2016-12-08 22:18:21 +0100

    add docstrings to get_pronunciations

--- pronunciation_master/get_pronunciations.py
+++ pronunciation_master/get_pronunciations.py
@@ -9,6 +9,15 @@ from wiktionaryparser import WiktionaryParser
 import language_codes
 
 def get_pronunciations(language, word):
+    """ main function of module,
+    gets the pronunciations for a 'word' in a 'language'
+    dependency on Wiktionary (get_wiktionary_entry) to get this
+    Arguments:
+        language = language to get pronunciations for
+        word = word to get the pronunciations for
+    Returns:
+        set of pronunciations set('pron1', 'pron2', ...)
+    """
     language_code = language_codes.Wiktionary.map(language)
     wiktionary_entry = get_wiktionary_entry(language_code, word)
     pronunciation_entries = filter_pronunciations(wiktionary_entry)
@@ -16,17 +25,36 @@ def get_pronunciations(language, word):
     return pronunciations
 
 def get_wiktionary_entry(language, word):
+    """Interface to the requestion something from wiktionary.
+    Arguments:
+        language = language of which we want the entry
+        word = word  of which we want the entry
+    Returns:
+        parsed wiktionary page
+    """
     parser = WiktionaryParser()
     parser.set_default_language(language)
     return parser.fetch(word)
 
 def filter_pronunciations(wiktionary_entry):
+    """ filters out only the pronunciation entries of a wikipedia page
+    Arguments:
+        wiktionary_entry = parsed wiktionary page
+    Returns:
+        iteratable of pronunciation entries : (entry1; entry2, ...)
+    """
     key = 'pronunciations'
     pronunciation_entries = (entry.get(key, []) for entry in wiktionary_entry)
     pronunciations = itertools.chain.from_iterable(pronunciation_entries)
     return pronunciations
 
 def list_pronunciations(pronunciation_entries):
+    """ Parses all the pronunciations from the entries
+    Arguments:
+        pronunciation_entries = a iteratable of entries to parse
+    Returns:
+        set of pronunciations : set(pron1, pron2, ...)
+    """
     pronunciations = set()
     for entry in pronunciation_entries:
         m = re.search('IPA: */(.*)/', entry)
@@ -49,11 +77,15 @@ def _parse_arguments():
                         help='the word we want the pronunciations for')
     return parser.parse_args()
 
-
-if __name__ == '__main__':
+def main():
     args = _parse_arguments()
     pronunciations = get_pronunciations(args.language, args.word)
     if not pronunciations:
-        raise RuntimeError("No pronunciations found for word '{}' in language '{}'".format(word, language))
+        message = "No pronunciations found for word '{}' in language '{}'"
+        raise RuntimeError(message.format(word, language))
     for pronunciation in pronunciations:
         print(pronunciation)
+
+
+if __name__ == '__main__':
+    main()
