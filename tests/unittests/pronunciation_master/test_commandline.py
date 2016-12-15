e6cbba981e5afff19cae1448b84739b52ace2978
Jeroen Decroos <jeroen.decroos@gmail.com>
2016-12-15 19:44:24 +0100

    add some test for pronunciation_master.commandline

---
+++ tests/unittests/pronunciation_master/test_commandline.py
@@ -0,0 +1,26 @@
+import tests.testlib.testcase as testcase
+import pronunciation_master.commandline as commandline
+
+class ArgumentParserTest(testcase.BaseTestCase):
+
+    def test_init(self):
+        commandline.ArgumentParser('description')
+
+    def test_add_language(self):
+        parser = commandline.ArgumentParser()
+        parser.add_language()
+        args = parser.parse_args(['--language', 'd'])
+        self.assertEqual(args.language, 'd')
+
+    def test_add_word(self):
+        parser = commandline.ArgumentParser()
+        parser.add_word()
+        args = parser.parse_args(['--word', 'd'])
+        self.assertEqual(args.word, 'd')
+    
+    def test_wrong_argument(self):
+        parser = commandline.ArgumentParser()
+        with self.assertRaises(SystemExit):
+            parser.parse_args(['--something', 'd'])
+
+
