468bfa2555924a4ee39ced46a260bfcabfa8db4c
Jeroen Decroos <jeroen.decroos@gmail.com>
2016-12-23 16:50:20 +0100

    Issue 7: refactor language codes

--- pronunciation_master/src/language_codes.py
+++ pronunciation_master/src/language_codes.py
@@ -1,6 +1,40 @@
 import abc
 
 
+class ISO639(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __getitem__(self, key):
+        """ This method should return a isoX value for the language key
+        """
+
+    @abc.abstractmethod
+    def __contains__(self, key):
+        """ This method should check if it has an ISOX entry language key
+        """
+
+
+class ISO639_2(ISO639):
+    codes = {'dutch': 'nl'}
+
+    def __getitem__(self, key):
+        return self.codes[key]
+
+    def __contains__(self, key):
+        return key in self.codes
+
+
+class ISO639_3(ISO639):
+    codes = {'dutch': 'nld'}
+
+    def __getitem__(self, key):
+        return self.codes[key]
+
+    def __contains__(self, key):
+        return key in self.codes
+
+
 class LanguageCodes(object):
     """abstract class to map different language codes to each other
     sub classes need to implement the code-mapping
@@ -23,13 +57,13 @@ class LanguageCodes(object):
 class HermitDave(LanguageCodes):
     """ mapping to serve HermitDave word frequency mapping
     """
-    codes = {'dutch': 'nl'}
+    codes = ISO639_2()
 
 
 class Phoibe(LanguageCodes):
     """ mapping to serve phoibe.com
     """
-    codes = {'dutch': 'nld'}
+    codes = ISO639_3()
 
 
 class Wiktionary(LanguageCodes):
